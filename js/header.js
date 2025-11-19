f0bd7dc59f9d6aaa7fd3ecfd2946651e8b17852a
xhelaledin <xhelaledin@gmail.com>
2025-11-19 10:47:33 +0100

    Enhance styling and structure; add header and script files for improved functionality

---
+++ js/header.js
@@ -0,0 +1,11 @@
+/* <header>
+      <h1><a href="index.html">Our Group</a></h1>
+      <nav>
+        <ul>
+          <li><a href="index.html class="active">Home</a></li>
+          <li><a href="member1.html">Enes Gjurgjiali</a></li>
+          <li><a href="member2.html">Xhelaledin Bakiç</a></li>
+        </ul>
+      </nav>
+    </header>
+ */
